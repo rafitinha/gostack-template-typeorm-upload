de440e514bf4816e3584eef7ef79ec64a9bf4985
Rafael Cruz Oliviera <rafael.cruz@geq.com.br>
2021-06-05 21:54:15 -0300

    Alterado nome do arquivo de import para file

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -41,7 +41,7 @@ transactionsRouter.delete('/:id', async (request, response) => {
 
 transactionsRouter.post(
   '/import',
-  upload.single('import'),
+  upload.single('file'),
   async (request, response) => {
     const importTransactionsService = new ImportTransactionsService();
     const arrayTransactions = await importTransactionsService.execute(
